1743591d44c2e721c0187b293d198284894d0272
Alexandre Bruneau <alexandrebruneau@outlook.com>
2021-10-13 16:01:22 +0200

    Ajout cursus  & stagiaires controllers

---
+++ LaWebAPIFactory/Controllers/CursusController .cs
@@ -0,0 +1,145 @@
+﻿using LaWebAPIFactory.Context;
+using LaWebAPIFactory.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace LaWebAPIFactory.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CursusController : ControllerBase
+    {
+        private readonly FactoryContext _context;
+        public CursusController(FactoryContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/<CursusController>
+        [HttpGet]
+        public ActionResult<IEnumerable<Cursus>> Get()
+        {
+            return _context.Cursus.Include(m => m.Stagiaires).ToList();
+        }
+
+        // GET api/<CursusController>/5
+        [HttpGet("{id}")]
+        public ActionResult<Cursus> Get(int id)
+        {
+            var cursus = _context.Cursus.Where(m => m.Id == id).Include(m => m.Stagiaires).SingleOrDefault();
+
+            if (cursus == null)
+            {
+                return NotFound();
+            }
+
+            return cursus;
+        }
+
+        // GET api/<CursusController>/5
+        [HttpGet("{id}/custom")]
+        public ActionResult<dynamic> GetCustom(int id)
+        {
+            var cursus = _context.Cursus.Find(id);
+
+            if (cursus == null)
+            {
+                return NotFound();
+            }
+
+            return new
+            {
+                Identifiant = cursus.Id,
+                Intitule = cursus.Intitule
+            };
+        }
+
+        // GET api/<CursusController>/5
+        //[HttpGet("{id}/customVM")]
+        //public ActionResult<CursusVM> GetCustomVM(int id)
+        //{
+        //    var Cursus = _context.Cursus.Where(m => m.Id == id).Include(m => m.Stagiaires).SingleOrDefault();
+
+        //    if (Cursus == null)
+        //    {
+        //        return NotFound();
+        //    }
+
+        //    CursusVM CursusVM = new CursusVM(Cursus.Id, Cursus.Titre);
+
+        //    foreach(Competence comp in Cursus.Competences)
+        //    {
+        //        CursusVM.FormateursId.Add(comp.FormateurId);
+        //    }
+
+        //    return CursusVM;
+        //}
+
+        // POST api/<CursusController>
+        [HttpPost]
+        public ActionResult<Cursus> Post([FromBody] Cursus cursus)
+        {
+            _context.Cursus.Add(cursus);
+            _context.SaveChanges();
+
+            return CreatedAtAction("GetCursus", new { id = cursus.Id }, cursus);
+        }
+
+        // PUT api/<CursusController>/5
+        [HttpPut("{id}")]
+        public IActionResult Put(int id, [FromBody] Cursus cursus)
+        {
+            if (id != cursus.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(cursus).State = EntityState.Modified;
+
+            try
+            {
+                _context.SaveChanges();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!CursusExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return Ok();
+        }
+
+        // DELETE api/<CursusController>/5
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id)
+        {
+            var cursus = _context.Cursus.Find(id);
+            if (cursus == null)
+            {
+                return NotFound();
+            }
+
+            _context.Cursus.Remove(cursus);
+            _context.SaveChanges();
+
+            return NoContent();
+        }
+
+        private bool CursusExists(int id)
+        {
+            return _context.Cursus.Any(e => e.Id == id);
+        }
+    }
+}
---
+++ LaWebAPIFactory/Controllers/StagiairesController.cs
@@ -0,0 +1,146 @@
+﻿using LaWebAPIFactory.Context;
+using LaWebAPIFactory.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace LaWebAPIFactory.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StagiairesController : ControllerBase
+    {
+        private readonly FactoryContext _context;
+        public StagiairesController(FactoryContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/<StagiairesController>
+        [HttpGet]
+        public ActionResult<IEnumerable<Stagiaire>> Get()
+        {
+            return _context.Stagiaires.ToList();
+        }
+
+        // GET api/<StagiairesController>/5
+        [HttpGet("{id}")]
+        public ActionResult<Stagiaire> Get(int id)
+        {
+            var stagiaire = _context.Stagiaires.Where(m => m.Id == id).SingleOrDefault();
+
+            if (stagiaire == null)
+            {
+                return NotFound();
+            }
+
+            return stagiaire;
+        }
+
+        // GET api/<StagiairesController>/5
+        [HttpGet("{id}/custom")]
+        public ActionResult<dynamic> GetCustom(int id)
+        {
+            var stagiaire = _context.Stagiaires.Find(id);
+
+            if (stagiaire == null)
+            {
+                return NotFound();
+            }
+
+            return new
+            {
+                Identifiant = stagiaire.Id,
+                Prenom = stagiaire.Prenom,
+                Nom = stagiaire.Nom,
+            };
+        }
+
+        // GET api/<StagiairesController>/5
+        //[HttpGet("{id}/customVM")]
+        //public ActionResult<StagiairesVM> GetCustomVM(int id)
+        //{
+        //    var Stagiaires = _context.Stagiaires.Where(m => m.Id == id).Include(m => m.Stagiaires).SingleOrDefault();
+
+        //    if (Stagiaires == null)
+        //    {
+        //        return NotFound();
+        //    }
+
+        //    StagiairesVM StagiairesVM = new StagiairesVM(Stagiaires.Id, Stagiaires.Titre);
+
+        //    foreach(Competence comp in Stagiaires.Competences)
+        //    {
+        //        StagiairesVM.FormateursId.Add(comp.FormateurId);
+        //    }
+
+        //    return StagiairesVM;
+        //}
+
+        // POST api/<StagiairesController>
+        [HttpPost]
+        public ActionResult<Stagiaire> Post([FromBody] Stagiaire stagiaire)
+        {
+            _context.Stagiaires.Add(stagiaire);
+            _context.SaveChanges();
+
+            return CreatedAtAction("GetStagiaires", new { id = stagiaire.Id }, stagiaire);
+        }
+
+        // PUT api/<StagiairesController>/5
+        [HttpPut("{id}")]
+        public IActionResult Put(int id, [FromBody] Stagiaire stagiaire)
+        {
+            if (id != stagiaire.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(stagiaire).State = EntityState.Modified;
+
+            try
+            {
+                _context.SaveChanges();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!StagiairesExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return Ok();
+        }
+
+        // DELETE api/<StagiairesController>/5
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id)
+        {
+            var stagiaire = _context.Stagiaires.Find(id);
+            if (stagiaire == null)
+            {
+                return NotFound();
+            }
+
+            _context.Stagiaires.Remove(stagiaire);
+            _context.SaveChanges();
+
+            return NoContent();
+        }
+
+        private bool StagiairesExists(int id)
+        {
+            return _context.Stagiaires.Any(e => e.Id == id);
+        }
+    }
+}
